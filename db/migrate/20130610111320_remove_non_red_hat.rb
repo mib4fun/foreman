28ba95822eb69e2d487bb696d1221b9fce150575
Sam Kottler <shk@redhat.com>
2013-06-10 12:05:51 -0400

    Fixes BZ#971463 - remove non-Red Hat default content
    (cherry picked from commit dd1c905bc28b76b3400a933656e4c29f592a8d32)

---
+++ db/migrate/20130610111320_remove_non_red_hat.rb
@@ -0,0 +1,24 @@
+# BZ#971463 - remove non-Red Hat default content
+class RemoveNonRedHat < ActiveRecord::Migration
+  def up
+    Medium.destroy_all
+
+    ConfigTemplate.all.each do |tmpl|
+      next if tmpl.snippet? && tmpl.name != 'vmware'
+      next if tmpl.name.start_with? 'PXE'
+      next if tmpl.name.include? 'Kickstart'
+      next if tmpl.name.include? 'Grubby'
+      tmpl.destroy
+    end
+
+    rhr = ConfigTemplate.find_by_name('redhat_register')
+    rhr.template = rhr.template.gsub('spacewalk', 'satellite').gsub('Spacewalk/Satellite', 'Satellite').gsub('Spacewalk', 'Satellite')
+    rhr.save!
+
+    Ptable.destroy_all('name NOT LIKE "RedHat%"')
+  end
+
+  def down
+    # not implemented
+  end
+end

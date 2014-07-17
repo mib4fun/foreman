f6e3beed0cec514481e9268f4933e000c7bcbf8d
Stephen Benjamin <stephen@bitbin.de>
2014-07-17 14:29:20 +0200

    fixes #1646, #3103 - enable cloning and locking of templates

--- app/helpers/config_templates_helper.rb
+++ app/helpers/config_templates_helper.rb
@@ -26,4 +26,22 @@ module ConfigTemplatesHelper
     (_("The final entry, Operating System default, can be set by editing the %s page.") % (link_to _("Operating System"), operatingsystems_path)).html_safe
   end
 
+  def permitted_actions config_template
+    actions = [display_link_if_authorized(_('Clone'), hash_for_clone_config_template_path(:id => config_template))]
+
+    if config_template.locked?
+      # Default templates aren't actually unlockable - show the link as gray and no confirm dialog.
+      opts = config_template.default ? {:style => "color: gray"} : {:confirm =>  _("You are about to unlock a locked " \
+        "template -- this is for every organization and location that uses it. Continue?"), :style => "color: red"}
+
+      actions << display_link_if_authorized(_('Unlock'), hash_for_unlock_config_template_path(:id => config_template), opts) 
+
+    else
+      actions << display_link_if_authorized(_('Lock'), hash_for_lock_config_template_path(:id => config_template))
+      actions << display_delete_if_authorized(hash_for_config_template_path(:id => config_template.to_param).
+         merge(:auth_object => config_template, :authorizer => authorizer, :permission => 'destroy_templates'),
+         :confirm => _("Delete %s?") % config_template)
+    end
+  end
 end
+

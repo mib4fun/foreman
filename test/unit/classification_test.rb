d59027ca3c1fc18bb8f14c7070def9ba897bbad6
Marek Hulan <mhulan@redhat.com>
2015-03-09 14:30:17 +0200

    Fixes #9673 - treat YAML and JSON lookup keys as strings
    (cherry picked from commit 25a7f806ed586bd32ee647f2acd8e5a0224cbbfb)
    
    Conflicts:
    	test/unit/classification_test.rb

--- test/unit/classification_test.rb
+++ test/unit/classification_test.rb
@@ -120,22 +120,6 @@ class ClassificationTest < ActiveSupport::TestCase
     assert_includes values_hash[json_lkey.id][json_lkey.key][:value], '{"a":"b"}'
   end
 
-  test "#value_of_key should correctly typecast JSON and YAML default values" do
-    env = FactoryGirl.create(:environment)
-    pc = FactoryGirl.create(:puppetclass, :environments => [env])
-    yaml_lkey = FactoryGirl.create(:lookup_key, :as_smart_class_param,
-                                   :puppetclass => pc, :key_type => 'yaml', :default_value => 'a: b')
-    json_lkey = FactoryGirl.create(:lookup_key, :as_smart_class_param,
-                                   :puppetclass => pc, :key_type => 'json', :default_value => '{"a": "b"}')
-    classparam = Classification::ClassParam.new
-
-    yaml_value = classparam.send(:value_of_key, yaml_lkey, {})
-    json_value = classparam.send(:value_of_key, json_lkey, {})
-
-    assert_equal yaml_value, {'a' => 'b'}
-    assert_equal json_value, {'a' => 'b'}
-  end
-
   test 'smart class parameter of array with avoid_duplicates should return lookup_value array without duplicates' do
 
     key = FactoryGirl.create(:lookup_key, :as_smart_class_param,
@@ -686,6 +670,19 @@ class ClassificationTest < ActiveSupport::TestCase
 
     assert_raise RuntimeError do
       classification.enc['base'][key.key]
+    end
+  end
+
+  context 'lookup value type cast error' do
+    setup do
+      @lookup_key = mock('lookup_key')
+      @lookup_key.expects(:cast_validate_value).raises(TypeError)
+      @lookup_key.expects(:key_type).returns('footype')
+    end
+
+    test 'TypeError exceptions are logged' do
+      Rails.logger.expects(:warn).with('Unable to type cast bar to footype')
+      @classification.send(:type_cast, @lookup_key, 'bar')
     end
   end
 

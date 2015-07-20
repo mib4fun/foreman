ed460b2ee1915f6a6f2e661b345aa6f8323ebc55
Marek Hulan <mhulan@redhat.com>
2015-07-20 15:57:26 +0200

    BZ1225348

--- app/controllers/compute_attributes_controller.rb
+++ app/controllers/compute_attributes_controller.rb
@@ -6,6 +6,7 @@ class ComputeAttributesController < ApplicationController
   end
 
   def create
+    clear_deleted_records
     @set = ComputeAttribute.new(params[:compute_attribute])
     if @set.save
       process_success :success_redirect => request.referer || compute_profile_path(@set.compute_profile)
@@ -19,6 +20,7 @@ class ComputeAttributesController < ApplicationController
   end
 
   def update
+    clear_deleted_records
     @set = ComputeAttribute.find(params[:id])
     if @set.update_attributes!(params[:compute_attribute])
       process_success :success_redirect => request.referer || compute_profile_path(@set.compute_profile)
@@ -27,4 +29,27 @@ class ComputeAttributesController < ApplicationController
     end
   end
 
+  private
+
+  def clear_deleted_records
+    base = params['compute_attribute'].fetch('vm_attrs', {})
+    if (attrs = base.fetch('nics_attributes', {})).present?
+      delete_removed_records(attrs)
+    end
+    if (attrs = base.fetch('interfaces_attributes', {})).present?
+      delete_removed_records(attrs)
+    end
+    if (attrs = base.fetch('volumes_attributes', {})).present?
+      delete_removed_records(attrs)
+    end
+  end
+
+  def delete_removed_records(base)
+    base.each do |key, value|
+      if value['_delete'] == '1'
+        base.delete(key)
+      end
+    end
+
+  end
 end
